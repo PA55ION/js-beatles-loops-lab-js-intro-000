3d9b985b2f868a16db53dbce3733327944cb49fc
Mai Campbell <jirallc@gmail.com>
2019-04-24 19:07:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(n) {
     const arr = [];
 
-    do() {
+    do {
       arr.push('I love the Beatles!');
       n++;
     } while (n < 15)
-    
+
     return arr;
 }

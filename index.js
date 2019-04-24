0610ceba8a7a5eb513cb4718903cc4cbbcb67296
Mai Campbell <jirallc@gmail.com>
2019-04-24 19:22:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,12 @@ function johnLennonFacts(facts) {
     }
     return newFacts;
 }
+
+function iLoveTheBeatles(n) {
+    const arr = [];
+
+    do {
+      arr.push('I love the Beatles!')
+    } while (n < 15)
+      return arr;
+}
